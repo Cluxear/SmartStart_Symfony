75d68671ec7eb4f38f66c43ddf581b3c035b39bd
KryptixKM <mhedhbi.ka@gmail.com>
2019-04-19 08:32:20 +0100

    finished

---
+++ src/EvenementBundle/Entity/Participation.php
@@ -0,0 +1,147 @@
+<?php
+
+namespace EvenementBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Participation
+ *
+ * @ORM\Table(name="participation")
+ * @ORM\Entity(repositoryClass="EvenementBundle\Repository\ParticipationRepository")
+ */
+class Participation
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+    /**
+     * @ORM\Column(name="titre", type="string",nullable=true)
+     */
+    private $titre;
+    /**
+     * @ORM\Column(name="description", type="text",nullable=true)
+     */
+    private $description;
+    /**
+     * @ORM\Column(name="fichier", type="string")
+     */
+    private $fichier;
+    /**
+     * @ORM\ManyToOne(targetEntity="\UserBundle\Entity\User")
+     * @ORM\JoinColumn(name="user_id", referencedColumnName="id")
+     */
+    private $user_id;
+    /**
+     * @ORM\ManyToOne(targetEntity="Evenement")
+     * @ORM\JoinColumn(name="evenement_id", referencedColumnName="id")
+     */
+    private $evenement_id;
+
+    /**
+     * @return mixed
+     */
+    public function getTitre()
+    {
+        return $this->titre;
+    }
+
+    /**
+     * @param mixed $titre
+     */
+    public function setTitre($titre)
+    {
+        $this->titre = $titre;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param mixed $description
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFichier()
+    {
+        return $this->fichier;
+    }
+
+    /**
+     * @param mixed $fichier
+     */
+    public function setFichier($fichier)
+    {
+        $this->fichier = $fichier;
+    }
+
+
+    /**
+     * @return mixed
+     */
+    public function getUserId()
+    {
+        return $this->user_id;
+    }
+
+    /**
+     * @param mixed $user_id
+     */
+    public function setUserId($user_id)
+    {
+        $this->user_id = $user_id;
+    }
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId(int $id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEvenementId()
+    {
+        return $this->evenement_id;
+    }
+
+    /**
+     * @param mixed $evenement_id
+     */
+    public function setEvenementId($evenement_id)
+    {
+        $this->evenement_id = $evenement_id;
+    }
+
+
+}
+
